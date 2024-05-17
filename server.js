9ebe798cbe8b7a21ac1b1634da20e4659b63a5c2
SarahC233 <122423221+SarahC233@users.noreply.github.com>
2024-05-17 19:11:35 +1200

    Added CORS and new .env variable

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const fs = require("fs");
 require("dotenv").config();
+const cors =require("cors");      
 
 const app = express();
 
